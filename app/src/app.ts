21a08f2feb575dab86e14c77d2e2f8ad1408a14a
Jozias Martini <jozias.martini@gmail.com>
2022-03-01 02:56:18 -0300

    Start express and config libs

--- app/src/app.ts
+++ app/src/app.ts
@@ -1,1 +1,14 @@
-console.log("Funcionou");
+import express from "express"
+import config from "config"
+
+const app = express()
+
+// JSON Middleware
+app.use(express.json())
+
+// App Port
+const port = config.get<number>("port")
+
+app.listen(3000, async() => {
+    console.log(`Aplicação está funcionando na porta ${port}.`)
+})
