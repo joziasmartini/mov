67ce4225bfe1f69e0057175f4751eaa7eb53a7b2
Jozias Martini <jozias.martini@gmail.com>
2022-03-01 03:38:07 -0300

    Configure and connect MongoDB Atlas

--- app/config/default.ts
+++ app/config/default.ts
@@ -1,3 +1,4 @@
 export default {
     port: 3000,
+    dbUri: "mongodb+srv://joziasmartini:JZS898989@cluster-one.smhpk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 }
--- app/src/app.ts
+++ app/src/app.ts
@@ -6,6 +6,9 @@ const app = express()
 // JSON Middleware
 app.use(express.json())
 
+// Database
+import db from "../config/db"
+
 // Routes
 import router from "./router"
 
@@ -15,5 +18,7 @@ app.use("/api/", router)
 const port = config.get<number>("port")
 
 app.listen(3000, async() => {
+    await db();
+    
     console.log(`Aplicação está funcionando na porta ${port}.`)
 })
